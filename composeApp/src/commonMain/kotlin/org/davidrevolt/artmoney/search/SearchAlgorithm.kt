d5679f30ebfc999b61195c193b97c71dcfa1c474
DaViD <DaViD@DESKTOP-53JG5B1>
2025-03-23 00:55:42 +0200

    ArtMoney

--- composeApp/src/commonMain/kotlin/org/davidrevolt/artmoney/search/SearchAlgorithm.kt
+++ composeApp/src/commonMain/kotlin/org/davidrevolt/artmoney/search/SearchAlgorithm.kt
@@ -7,6 +7,12 @@ interface SearchAlgorithm {
      * Given memory buffer: [0x00, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x90] ,starts at baseAddress
      * Searching for target : [0x12, 0x34]
      * Result: [baseAddress + 1, baseAddress + 5]
+     *
+     * Search algorithm should handle overlapping, e.g:
+     * Buffer:  A  B  A  B  A
+     * Index:   0  1  2  3  4
+     * Pattern: A  B  A
+     * Should return indexes [0, 2]
      */
     fun search(buffer: ByteArray, baseAddress: Long, target: ByteArray): List<Long>
 }
